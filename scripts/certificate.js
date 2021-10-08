ebf7e42b61926608f0a5bc6e76c0fcecf9027a58
Mike Koss <mike@mckoss.com>
2021-10-07 23:07:14 -0700

    Basic cert and formatting.

--- scripts/certificate.js
+++ scripts/certificate.js
@@ -1,1 +1,30 @@
-fetch("./data/2021.json").then((d) => console.log(d));
+let params = new URLSearchParams(document.location.search.substring(1));
+const CERT_ID = params.get('id');
+const [CERT_YEAR, CERT_NUM] = CERT_ID.split('-');
+
+DATA_URL = `./data/${CERT_YEAR}.json`;
+
+async function parseCert() {
+    let response = await fetch(DATA_URL);
+    if (!response.ok) {
+        return;
+    }
+    let certs = await response.json();
+    if (certs[CERT_NUM] === undefined) {
+        return;
+    }
+    document.body.className = 'cert';
+    bindParams(certs[CERT_NUM]);
+}
+
+function bindParams(data) {
+    for (let prop in data) {
+        let span = document.getElementById(prop);
+        if (span !== null) {
+            span.textContent = data[prop];
+        }
+    }
+}
+
+
+parseCert();
